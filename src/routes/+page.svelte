34166999fe48675bfbec3ec7baa42796f6ac0a04
Gaven Behrends <gzbehrends@gmail.com>
2024-04-12 21:04:42 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let count = 0;
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Click the button to complete the project faster</p>
+
+<button on:click={()=>{
+    count += 1;
+}}>
+    {count}% completion 
+</button>
