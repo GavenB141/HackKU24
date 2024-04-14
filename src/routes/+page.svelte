9a5f2ebd1d6b345c1db213fd2cf4fe9386ef8610
EJGao11 <e784g298@home.ku.edu>
2024-04-14 00:24:02 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,30 @@
     let count = 0;
 </script>
 
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Click the button to complete the project faster</p>
+<style>
+    .simpleGradient {
+        background: linear-gradient(black, darkgray);   
+    }
+    .center {
+        margin: 0;
+        position: absolute;
+        top: 85%;
+        left: 50%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+    .bit {
+        color: #11a9BC;
+    }
+    .pinch {
+        color: #84D6D7; 
+    }
+</style>
+    <h1 class="w-full text-center font-black font-mono mt-1">
+    <span class="bit">bit</span><span class="pinch">pinch</span>
+</h1>
+<p>
+<p style="font-size:36px" >  Enage with the Highest Income and Outcome Trading Simulator</p>
 
 <button on:click={()=>{
     count += 1;
@@ -17,5 +37,5 @@
 <p>This should be a nice introductory page to our application and why it's cool.</p>
 <a
     data-sveltekit-reload
-    href="/dashboard" class="rounded py-2 bg-blue-800 text-white no-underline mt-2 block text-center"
+    href="/dashboard" class="center w-1/2 rounded py-4 bg-blue-500 text-white no-underline mt-4 block text-center"
 >Get Started</a>
