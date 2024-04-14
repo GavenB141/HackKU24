ee43bf3a38591014bb2b320b7d5f0c9511525c2e
mmann10190 <m370m615@ku.edu>
2024-04-14 02:31:12 -0500

    landing page
    i dislike css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,55 @@
-<script>
-    let count = 0;
-</script>
-
 <style>
-    .simpleGradient {
-        background: linear-gradient(black, darkgray);   
-    }
-    .center {
+    .logo {
         margin: 0;
         position: absolute;
-        top: 85%;
+        top: 36%;
+        left: 20%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+    .button {
+        color: black;
+        font-size: 40px;
+        margin: 0;
+        position: absolute;
+        top: 90%;
         left: 50%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+    .bitpinch {
+        margin: 0;
+        position: absolute;
+        top: 25%;
+        left: 70%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+    .tag1 {
+        text-align: center;
+        font-size: 36px;
+        margin: 0;
+        position: absolute;
+        top: 45%;
+        left: 70%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+    .duc {
+        margin: 0;
+        position: absolute;
+        bottom: 0%;
+        right: 10%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+    .hackku {
+        text-align: center;
+        font-size: 36px;
+        margin: 0;
+        position: absolute;
+        top: 95%;
+        right: -4.2%;
         -ms-transform: translate(-50%, -50%);
         transform: translate(-50%, -50%);
     }
@@ -21,21 +60,21 @@
         color: #84D6D7; 
     }
 </style>
-    <h1 class="w-full text-center font-black font-mono mt-1">
+
+<p class="hackku" style="font-size:24px">
+    Made for HACKKU
+</p>
+
+<h1 class="bitpinch font-black font-mono mt-1" style="font-size:128px">
     <span class="bit">bit</span><span class="pinch">pinch</span>
 </h1>
-<p>
-<p style="font-size:36px" >  Enage with the Highest Income and Outcome Trading Simulator</p>
 
-<button on:click={()=>{
-    count += 1;
-}}>
-    {count}% completion 
-</button>
-
-<h2>To Do</h2>
-<p>This should be a nice introductory page to our application and why it's cool.</p>
+<p class="tag1" style="width: 800px;">The Best Massivley Multiplayer Real Time Strategic Speculative Asset Market Simulation Game</p>
+<img src='src/lib/assets/logo.svg' width="25%" height="25%" class="logo">
+<img src='src/lib/assets/duc.svg' width="3%" height="3%" class="duc">
+   
 <a
     data-sveltekit-reload
-    href="/dashboard" class="center w-1/2 rounded py-4 bg-blue-500 text-white no-underline mt-4 block text-center"
+    href="/dashboard"
+    class="button w-1/5 rounded py-2 bg-blue-500 font-weight 900 no-underline mt-4 block text-center" 
 >Get Started</a>
