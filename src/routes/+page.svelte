6c22354cbaec64d33b0c45885361e9e7de3f0afe
Alex Rawson <ajzecrom@gmail.com>
2024-04-14 03:13:15 -0500

    do more flexboxing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,80 +1,52 @@
+<main class="h-screen w-screen" style="padding: 20vh;">
+    <div class="flex flex-col justify-end h-full">
+        <div class="flex flex-col md:flex-row justify-evenly">
+            <img
+                src="src/lib/assets/logo.svg"
+                alt="Bitpinch Logo"
+                class="w-full h-full max-w-prose"
+            />
+            <div class="flex flex-col max-w-prose">
+                <h1 class="text-8xl font-black font-mono mt-1 mb-4">
+                    <span class="bit">bit</span><span class="pinch">pinch</span>
+                </h1>
+                <p class="text-2xl">
+                    The Massively Multiplayer Real Time Strategic Speculative
+                    Asset Market Simulation Game
+                </p>
+            </div>
+        </div>
+        <div class="flex h-full w-full justify-center content-center">
+            <a
+                data-sveltekit-reload
+                href="/dashboard"
+                class="button w-1/5 rounded py-2 bg-blue-500 font-weight 900 no-underline mt-4 block text-center text-4xl m-auto min-w-fit"
+                >Get Started</a
+            >
+        </div>
+    </div>
+</main>
+<div class="flex hackku-bottom-right">
+    <img src="src/lib/assets/duc.svg" style="height: 150%" />
+    <span>Made for HACKKU</span>
+</div>
+
 <style>
-    .logo {
-        margin: 0;
-        position: absolute;
-        top: 36%;
-        left: 20%;
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
-    .button {
-        color: black;
-        font-size: 40px;
-        margin: 0;
-        position: absolute;
-        top: 90%;
-        left: 50%;
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
-    .bitpinch {
-        margin: 0;
-        position: absolute;
-        top: 25%;
-        left: 70%;
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
-    .tag1 {
-        text-align: center;
-        font-size: 36px;
-        margin: 0;
-        position: absolute;
-        top: 45%;
-        left: 70%;
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
-    .duc {
-        margin: 0;
-        position: absolute;
-        bottom: 0%;
-        right: 10%;
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
-    .hackku {
-        text-align: center;
-        font-size: 36px;
-        margin: 0;
-        position: absolute;
-        top: 95%;
-        right: -4.2%;
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
     .bit {
-        color: #11a9BC;
+        color: #11a9bc;
     }
     .pinch {
-        color: #84D6D7; 
+        color: #84d6d7;
+    }
+    .hackku-bottom-right {
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        width: fit-content;
+        align-content: center;
+        justify-content: right;
+        font-size: 24px;
+        height: 1em;
+        margin: 0 0 1em 4em;
     }
 </style>
-
-<p class="hackku" style="font-size:24px">
-    Made for HACKKU
-</p>
-
-<h1 class="bitpinch font-black font-mono mt-1" style="font-size:128px">
-    <span class="bit">bit</span><span class="pinch">pinch</span>
-</h1>
-
-<p class="tag1" style="width: 800px;">The Best Massively Multiplayer Real Time Strategic Speculative Asset Market Simulation Game</p>
-<img src='src/lib/assets/logo.svg' width="25%" height="25%" class="logo">
-<img src='src/lib/assets/duc.svg' width="3%" height="3%" class="duc">
-   
-<a
-    data-sveltekit-reload
-    href="/dashboard"
-    class="button w-1/5 rounded py-2 bg-blue-500 font-weight 900 no-underline mt-4 block text-center" 
->Get Started</a>
