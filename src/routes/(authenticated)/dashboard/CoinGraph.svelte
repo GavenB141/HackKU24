7248a221148ae516fd2a9f45438b1854ffafa700
Alex Rawson <ajzecrom@gmail.com>
2024-04-14 00:09:18 -0500

    weak but synchronized stochastic chart generation

--- src/routes/(authenticated)/dashboard/CoinGraph.svelte
+++ src/routes/(authenticated)/dashboard/CoinGraph.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
     import GraphData from "$lib/components/GraphData";
     import LineGraph from "$lib/components/LineGraph.svelte";
-    import { onMount } from "svelte";
-
+    import { MessageSource } from "$lib/messageSource";
+    import { bearerToken } from "$lib/userData";
+    
     let graph = new GraphData([10], 50);
-
-    onMount(()=>{
-        let interval = setInterval(()=>{
-            graph.append(graph.latest + Math.random() * 100 - 50);
-        }, 1000);
-
-        return ()=>{
-            clearInterval(interval);
-        }
-    });
+    $: if ($bearerToken) {
+        new MessageSource("/api/graph", $bearerToken, (value) => {
+            graph.append(value);
+        });
+    };
 </script>
 
 <LineGraph graphData={graph} />
---
+++ src/routes/api/graph/[[table]]/+server.ts
@@ -0,0 +1,40 @@
+import { MessageSink } from '$lib/messageSink.js';
+import { verifyAuth } from '$lib/user.js'
+
+const DATA_POINTS_TO_SEND = 55;
+const history = new Array(DATA_POINTS_TO_SEND);
+history.fill(0);
+
+let idx = 0;
+
+export async function GET({request, params}) {
+    let user = await verifyAuth(request.headers);
+    if (!user) {
+        return new Response("you are not logged in", {
+            status: 401,
+        });
+    }
+    let sink = new MessageSink();
+    setTimeout(() => processStream(sink), 500);
+    return sink.newStream();
+}
+
+const SINKS: MessageSink[] = [];
+
+async function processStream(sink: MessageSink) {
+    for (let i = idx + 1; i != idx; i = (i + 1) % DATA_POINTS_TO_SEND) {
+        sink.sendMessage(history[i]);
+    }
+    SINKS.push(sink);
+}
+
+// a trivial made-up model of the market's history
+setInterval(() => {
+    let lastIdx = idx == 0 ? DATA_POINTS_TO_SEND - 1 : idx - 1;
+    let value = Math.max(history[lastIdx] + (Math.max(history[lastIdx] * 0.10, 1) * (Math.random() - 0.5)), 0);
+    history[idx] = value;
+    SINKS.forEach((sink) => {
+        sink.sendMessage(value);
+    });
+    idx = (idx + 1) % DATA_POINTS_TO_SEND;
+}, 100);
