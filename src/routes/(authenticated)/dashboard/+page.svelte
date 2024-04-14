df0e8f25579ddb296f2cce28e860e301c762ca16
Gaven Behrends <gzbehrends@gmail.com>
2024-04-13 19:32:58 -0500

    Groovin

--- src/routes/(authenticated)/dashboard/+page.svelte
+++ src/routes/(authenticated)/dashboard/+page.svelte
@@ -57,44 +57,44 @@
 <svelte:window bind:innerWidth={screenWidth} />
 
 <!-- General layout concept -->
-<div class="flex flex-row h-screen">
-    {#if !mobile}
-        <Sidebar {...data} />
-    {/if}
-    <div class="flex flex-col w-full">
-        <Topbar {...data} {mobile} bind:sidebarOpen={sidebarOpen} />
-        {#if mobile}
-            <Popup bind:open={sidebarOpen}>
-                <Sidebar {...data} />
-            </Popup>
-        {/if}
-        <div class="grid grid-cols-1 md:grid-cols-2 md:h-full">
-            <div class="border border-sky-400 w-full md:h-full p-2">Net worth graph
-
-                <!-- Temporary placement of temporary chat window -->
-                <div id="chat-window">
-                    <form on:submit|preventDefault={send_message}>
-                        <input name="message" class="text-black" bind:value={message} autocomplete="off">
-                        <input type="submit" value="Send Message">
-                    </form>
-                    <ul class="list-disc">
-                        {#each message_log as message}
-                            <li>{message}</li>
-                        {/each}
-                    </ul>
-                </div>
-
-            </div>
-            <div class="border border-green-200 w-full md:h-full p-2">Selected coin graph
+<!-- <div class="flex flex-row h-screen"> -->
+    <!-- {#if !mobile} -->
+    <!--     <Sidebar {...data} /> -->
+    <!-- {/if} -->
+    <!-- <div class="flex flex-col w-full"> -->
+    <!--     <Topbar {...data} {mobile} bind:sidebarOpen={sidebarOpen} /> -->
+    <!--     {#if mobile} -->
+    <!--         <Popup bind:open={sidebarOpen}> -->
+    <!--             <Sidebar {...data} /> -->
+    <!--         </Popup> -->
+    <!--     {/if} -->
+        <!-- <div class="grid grid-cols-1 md:grid-cols-2 md:h-full"> -->
+            <!-- <div class="border border-sky-400 w-full md:h-full p-2">Net worth graph -->
+            <!---->
+            <!--     <!-- Temporary placement of temporary chat window -->
+            <!--     <div id="chat-window"> -->
+            <!--         <form on:submit|preventDefault={send_message}> -->
+            <!--             <input name="message" class="text-black" bind:value={message} autocomplete="off"> -->
+            <!--             <input type="submit" value="Send Message"> -->
+            <!--         </form> -->
+            <!--         <ul class="list-disc"> -->
+            <!--             {#each message_log as message} -->
+            <!--                 <li>{message}</li> -->
+            <!--             {/each} -->
+            <!--         </ul> -->
+            <!--     </div> -->
+            <!---->
+            <!-- </div> -->
+            <div class="border border-green-200 w-full md:h-full p-2">
                <LineGraph /> 
             </div>
-        </div>
-        <div class="grid grid-cols-1 md:grid-cols-3 md:h-full">
-            <div class="border border-blue-400 w-full md:h-full p-2">Create a coin</div>
-            <div class="border border-teal-400 w-full md:h-full p-2">Leaderboard</div>
-            <div class="border border-cyan-400 w-full md:h-full p-2">Top coins</div>
-        </div>
-        <!-- this won't actually be a <marquee/> later -->
-        <marquee>Social media mechanic quick view</marquee>
-    </div>
-</div>
+        <!-- </div> -->
+        <!-- <div class="grid grid-cols-1 md:grid-cols-3 md:h-full"> -->
+        <!--     <div class="border border-blue-400 w-full md:h-full p-2">Create a coin</div> -->
+        <!--     <div class="border border-teal-400 w-full md:h-full p-2">Leaderboard</div> -->
+        <!--     <div class="border border-cyan-400 w-full md:h-full p-2">Top coins</div> -->
+        <!-- </div> -->
+        <!-- <!-- this won't actually be a <marquee/> later -->
+        <!-- <marquee>Social media mechanic quick view</marquee> -->
+    <!-- </div> -->
+<!-- </div> -->
